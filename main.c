9979ca8ad55fb3bd2548268f02428f3e8367a84a
eduardoabreu09 <39703095+eduardoabreu09@users.noreply.github.com>
2018-09-11 21:26:39 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,113 +1,154 @@
+/*
+ * This file is part of the ÂµOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2014 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ----------------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "diag/Trace.h"
+
+#include "Timer.h"
+#include "BlinkLed.h"
+void mymemset ( unsigned long * ptr, unsigned char value, unsigned char num );
+void mymemcpy ( unsigned long * ptr, unsigned long * alo, unsigned char num );
+int mymemcmp ( unsigned long * ptr, unsigned long * alo, unsigned char num );
 
 
-    void SetBit(unsigned long *p, unsigned char bitn){
 
-        unsigned char mascara = (1 << bitn);
+// ----------------------------------------------------------------------------
+//
+// Standalone STM32F0 led blink sample (trace via DEBUG).
+//
+// In debug configurations, demonstrate how to print a greeting message
+// on the trace device. In release configurations the message is
+// simply discarded.
+//
+// Then demonstrates how to blink a led with 1 Hz, using a
+// continuous loop and SysTick delays.
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the DEBUG output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+// The external clock frequency is specified as a preprocessor definition
+// passed to the compiler via a command line option (see the 'C/C++ General' ->
+// 'Paths and Symbols' -> the 'Symbols' tab, if you want to change it).
+// The value selected during project creation was HSE_VALUE=8000000.
+//
+// Note: The default clock settings take the user defined HSE_VALUE and try
+// to reach the maximum possible system clock. For the default 8 MHz input
+// the result is guaranteed, but for other values it might not be possible,
+// so please adjust the PLL settings in system/src/cmsis/system_stm32f0xx.c
+//
 
-        *p = *p | mascara;
+// ----- Timing definitions -------------------------------------------------
 
+// Keep the LED on for 2/3 of a second.
+#define BLINK_ON_TICKS  (TIMER_FREQUENCY_HZ * 3 / 4)
+#define BLINK_OFF_TICKS (TIMER_FREQUENCY_HZ - BLINK_ON_TICKS)
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+int
+main(int argc, char* argv[])
+{	unsigned long ptr[4]= {1,2,3,4};
+	unsigned long alo[4]= {15,15,8,8};
+	int x;
+
+
+
+
+
+  // Send a greeting to the trace device (skipped on Release).
+  trace_puts("Hello ARM World!");
+
+  trace_printf("coloca 1 nas 4 posicoes do vetor 1,2,3,4 \n");
+  mymemset(&ptr,1,4);
+    for(int i =0;i<4;i++){
+	  trace_printf("%u ",ptr[i]);
+
+  }
+  trace_printf("\n");
+
+  trace_printf("copia as duas primeras posicoes do vetor 15,15,8,8 para o 1,1,1,1 \n");
+  mymemcpy(&ptr,&alo,2);
+  for(int i =0;i<4;i++){
+	  trace_printf("%u ",ptr[i]);
+
+  }
+  trace_printf("\n");
+
+  for(int i =0;i<4;i++){
+  	  trace_printf("%u ",alo[i]);
 
     }
-
-    void ResetBit(unsigned long *p, unsigned char bitn){
-
-        unsigned char mascara = ~(1 << bitn);
-
-        *p = *p & mascara;
-
-
-    }
-
-    void ToggletBit(unsigned long *p, unsigned char bitn){
-
-        unsigned char mascara = (1 << bitn);
-
-        *p = *p ^ mascara;
-
-
-    }
-
-    unsigned char  ReadBit(unsigned long *p, unsigned char bitn){
-
-        unsigned char bit;
-
-        bit = (*p >> bitn) & 1;
-
-        return bit;
+  trace_printf("\n");
+  trace_printf("compara os dois vetores \n");
+  x = mymemcmp(&ptr,&alo,4);
+  trace_printf("%d",x);
 
 
 
-
-    }
-
-    unsigned long  ReadBitSlice(unsigned long *p, unsigned char bitstart, unsigned char bitend){
-
-    unsigned long slice;
-    unsigned char size;
-
-    size = 31-bitend;
-    slice = (*p << size);
-    slice = (slice >> (size + bitstart));
-
-    return slice;
+  // At this stage the system clock should have already been configured
+  // at high speed.
 
 
-    }
+  timer_start();
 
-    unsigned char IsLittleEndian (void) {
+  blink_led_init();
+  
+  uint32_t seconds = 0;
 
-    unsigned int i = 1;
-    char *c = (char*)&i;
-
-    if (*c)
-       return 1;
-    else
-        return 0;
-
-
-    }
-
-    int main()
+  // Infinite loop
+  while (1)
     {
-        unsigned long p=10;
-        unsigned char c;
-        unsigned long s;
-        SetBit(&p,0); // bit 0 tem valor 0 anteriormente
-        printf("%lu \n",p); // terá que mostrar 11
-        ResetBit(&p,0); // bit 0 agora tem valor 1
-        printf("%lu \n",p); // 10
-        ResetBit(&p,1); // bit 1 vale 1
-        printf("%lu \n",p); // 8
-        ResetBit(&p,0); // bit 0 é 0
-        printf("%lu \n",p); // 8
-        ToggletBit(&p,1); // troca o bit 1 que é 0
-        printf("%lu \n",p); // 10
-        c= ReadBit(&p,3); // le o bit 3 que vale 1
-        printf("%u \n",c); // 1
-        c= ReadBit(&p,0); // le o bit 0 que é 0
-        printf("%u \n",c); // 0
-        s = ReadBitSlice(&p,1,3); // le dos bits 1 ao 3
-        printf("%lu \n",s); // 5
-        c = IsLittleEndian();
-        printf("%u \n",c);
+      blink_led_on();
+      timer_sleep(seconds == 0 ? TIMER_FREQUENCY_HZ : BLINK_ON_TICKS);
 
+      blink_led_off();
+      timer_sleep(BLINK_OFF_TICKS);
 
+      ++seconds;
 
-
-
-
-
-
-        return 0;
-
-
-
-
-
-
+      // Count seconds on the trace device.
 
     }
+  // Infinite loop, never return.
+}
 
+#pragma GCC diagnostic pop
 
+// ----------------------------------------------------------------------------
