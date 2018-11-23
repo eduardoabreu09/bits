f50badedd123594c5164afea787bb9ee0cd6428e
eduardoabreu09 <39703095+eduardoabreu09@users.noreply.github.com>
2018-11-22 23:06:48 -0200

    Add files via upload

---
+++ Relog/Inc/i2c-lcd.h
@@ -0,0 +1,11 @@
+
+
+void lcd_init (void);   // initialize lcd
+
+void lcd_send_cmd (char cmd);  // send command to the lcd
+
+void lcd_send_data (char data);  // send data to the lcd
+
+void lcd_send_string (char *str);  // send string to the lcd
+
+void lcd_send_float (float data, int numberofdigits);  // display float numbers on lcd
